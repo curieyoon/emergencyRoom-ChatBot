2e7dc6e43df917eb06ce35a8cdc66059047ef7a7
shin0112 <wnslcosltimo12@gamil.com>
2022-11-24 21:06:57 +0900

    modify emergency_api.js

--- emergency_api.js
+++ emergency_api.js
@@ -6,10 +6,13 @@ const convert = require("xml-js");
 var url = 'http://apis.data.go.kr/B552657/ErmctInfoInqireService/getSrsillDissAceptncPosblInfoInqire';
 var queryParams = '?' + encodeURIComponent('serviceKey') + '=52tXHgaW46YUpGn9k0r3IQrduIl6kBOl3Ta8Idra1%2BpPMYhL4qVCDu9itW8FVbDtMF4f9LAT9NJXEx7pvEJv%2FQ%3D%3D'; /* Service Key*/
 
-queryParams += '&' + encodeURIComponent('STAGE1') + '=' + '%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C'; /* encodeURIComponent('서울특별시')*/
-queryParams += '&' + encodeURIComponent('STAGE2') + '=' + '%EA%B0%95%EB%82%A8%EA%B5%AC'; /* */
+queryParams += '&' + encodeURIComponent('STAGE1') + '=' + encodeURIComponent('서울특별시'); /* */
+queryParams += '&' + encodeURIComponent('STAGE2') + '=' + encodeURIComponent('강남구'); /* */
+queryParams += '&' + encodeURIComponent('SM_TYPE') + '=' + encodeURIComponent(''); /* */
 queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
 queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /* */
+
+console.log(queryParams);
 
 request({
     url: url + queryParams,
