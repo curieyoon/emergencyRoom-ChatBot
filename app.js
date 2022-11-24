bb9dfa608949c56307177ef90b3cccc1f5e45aaa
KIM <vkn123@naver.com>
2022-11-24 15:33:43 +0900

    update app.js

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const Address = require('./Address.js');
 const express = require('express');
 const request = require('request');
 const TARGET_URL = 'https://api.line.me/v2/bot/message/reply'
-const TOKEN = '채널 토큰으로 변경'
+const TOKEN = 'YK56BfHFgpILrxRk1FZrdcouFguf5CBA5qxM3zfDH6N9jR/cfPxVdK1P9vZHAk69mZDSlSkXNLy25pzqJbXrn3y76hwmH0Kiuvx3OadFYcuidWSp7VYAj4SqJSljv/q5KoYAOE2il8jmQkf4bQKvXQdB04t89/1O/w1cDnyilFU='
 const fs = require('fs');
 const path = require('path');
 const HTTPS = require('https');
-const domain = "도메인 변경"
+const domain = "ec2-18-215-162-16.compute-1.amazonaws.com"
 const sslport = 23023;
 const bodyParser = require('body-parser');
 const app = express();
@@ -14,76 +14,52 @@ const app = express();
 
 console.log(Address.getAddress('석수동길'));
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.get('',(req,res)=> {
-    res.send('tset')
-})
-
+app.use(bodyParser.json());
 app.post('/hook', function (req, res) {
-  var eventObj = req.body.events[0];
-  var source = eventObj.source;
-  var message = eventObj.message;
-  // request log
-  console.log('======================', new Date() ,'======================');
-  console.log('[request]', req.body);
-  console.log('[request source] ', eventObj.source);
-  console.log('[request message]', eventObj.message);
-  request.post(
-      {
-          url: TARGET_URL,
-          headers: {
-              'Authorization': `Bearer ${TOKEN}`
-          },
-          json: {
-              "replyToken":eventObj.replyToken,
-              "messages":[
-                  {
-                      "type":"text",
-                      "text":"Hello, user"
-                  },
-                  {
-                      "type":"text",
-                      "text":"May I help you?"
-                  }
-              ]
-          }
-      },(error, response, body) => {
-          console.log(body)
-      });
-  
-  res.sendStatus(200);
+    var eventObj = req.body.events[0];
+    var source = eventObj.source;
+    var message = eventObj.message;
+    // request log
+    console.log('======================', new Date() ,'======================');
+    console.log('[request]', req.body);
+    console.log('[request source] ', eventObj.source);
+    console.log('[request message]', eventObj.message);
+    request.post(
+        {
+            url: TARGET_URL,
+            headers: {
+                'Authorization': `Bearer ${TOKEN}`
+            },
+            json: {
+                "replyToken":eventObj.replyToken,
+                "messages":[
+                    {
+                        "type":"text",
+                        "text":"Hello, user"
+                    },
+                    {
+                        "type":"text",
+                        "text":"May I help you?"
+                    }
+                ]
+            }
+        },(error, response, body) => {
+            console.log(body)
+        });
+    
+    res.sendStatus(200);
 });
-
-
-app.get('/keyboard', (req, res) => {
-  const data = {'type': 'text'}
-  res.json(data);
-});
-
-app.post('/message', (req, res) => {
-  const question = req.body.userRequest.utterance;
-  const goMain = '처음으로';
+try {
+    const option = {
+      ca: fs.readFileSync('/etc/letsencrypt/live/' + domain +'/fullchain.pem'),
+      key: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/' + domain +'/privkey.pem'), 'utf8').toString(),
+      cert: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/' + domain +'/cert.pem'), 'utf8').toString(),
+    };
   
-  if (question === '테스트') {
-    const data = {
-      'version': '2.0',
-      'template': {
-	    'outputs': [{
-	      'simpleText': {
-	        'text': '테스트'
-	      }
-	    }],
-	    'quickReplies': [{
-	      'label': goMain,
-	      'action': 'message',
-	      'messageText': goMain
-	    }]
-      }
-    }
+    HTTPS.createServer(option, app).listen(sslport, () => {
+      console.log(`[HTTPS] Server is started on port ${sslport}`);
+    });
+  } catch (error) {
+    console.log('[HTTPS] HTTPS 오류가 발생하였습니다. HTTPS 서버는 실행되지 않습니다.');
+    console.log(error);
   }
-  res.json(data);
-});
-
-app.listen(3000, () => console.log('node on 3000'));
