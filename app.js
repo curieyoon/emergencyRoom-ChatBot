d5e6365aef22ba4cb6e458f3cc0c2f16ce888a03
root <root@ip-172-31-18-241.ec2.internal>
2022-11-28 01:48:15 +0000

    update app.js

--- app.js
+++ app.js
@@ -6,24 +6,21 @@ const TOKEN = 'YK56BfHFgpILrxRk1FZrdcouFguf5CBA5qxM3zfDH6N9jR/cfPxVdK1P9vZHAk69m
 const fs = require('fs');
 const path = require('path');
 const HTTPS = require('https');
-const domain = "ec2-18-215-162-16.compute-1.amazonaws.com"
+const domain = "2019102158.oss2022chatbot.tk"
 const sslport = 23023;
 const bodyParser = require('body-parser');
 const app = express();
 
 
-console.log(Address.getAddress('석수동길'));
 
 app.use(bodyParser.json());
 app.post('/hook', function (req, res) {
     var eventObj = req.body.events[0];
-    var source = eventObj.source;
-    var message = eventObj.message;
+    //var source = eventObj.source;
+    //var message = eventObj.message;
     // request log
     console.log('======================', new Date() ,'======================');
-    console.log('[request]', req.body);
-    console.log('[request source] ', eventObj.source);
-    console.log('[request message]', eventObj.message);
+    console.log((Address.getAddress('석수동길')).road_address.address_name)
     request.post(
         {
             url: TARGET_URL,
@@ -31,17 +28,18 @@ app.post('/hook', function (req, res) {
                 'Authorization': `Bearer ${TOKEN}`
             },
             json: {
-                "replyToken":eventObj.replyToken,
+                "replyToken":eventObj.replyToken, //eventObj.replyToken
                 "messages":[
                     {
                         "type":"text",
-                        "text":"Hello, user"
+                        "text":address
                     },
                     {
                         "type":"text",
-                        "text":"May I help you?"
+                        "text":"맞나요?"
                     }
-                ]
+                ],
+                "status" : 1
             }
         },(error, response, body) => {
             console.log(body)
@@ -49,10 +47,17 @@ app.post('/hook', function (req, res) {
     
     res.sendStatus(200);
 });
+
+
+
+
+
+
 try {
     const option = {
-      key: fs.readFileSync('./rootca.key', 'utf8'),
-      cert: fs.readFileSync('./rootca.crt' ,'utf8'),
+        ca: fs.readFileSync('/etc/letsencrypt/live/' + domain +'/fullchain.pem'),
+        key: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/' + domain +'/privkey.pem'), 'utf8').toString(),
+        cert: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/' + domain +'/cert.pem'), 'utf8').toString(),
     };
   
     HTTPS.createServer(option, app).listen(sslport, () => {
