9988e7747a67793f24047737bd7e12f50dc1d5cc
홍예림 <hongyeee@khu.ac.kr>
2022-11-29 13:55:13 +0900

    sample

---
+++ sample.js
@@ -0,0 +1,44 @@
+const express= require('express');
+const app = express();
+var request = require('request');
+
+//필요한 주소 정보 (data 형식 확정 x )
+let address = {
+    "current_address" : { "address" : "현재 위치 주소", "x" : 127.1058342, "y" : 37.359708},
+    "number" : 10,
+    "hospital_data" :[
+        {"name" : "병원명", "address" : "병원 주소", "x" : 129.075986, "y" : 35.179470, "distance" : 0, "duration" : 0},
+        {"name" : "병원명", "address" : "병원 주소", "x" : 127.1058342, "y" : 37.359708, "distance" : 0, "duration" : 0}
+    ]
+}
+
+var options = {
+  'method': 'GET',
+  'url': 'https://naveropenapi.apigw.ntruss.com/map-direction-15/v1/driving?start='+address.current_address.x+','
+            +address.current_address.y+'&goal='+address.hospital_data[0].x+','+address.hospital_data[0].y+'&option=trafast',
+  'headers': {
+    'X-NCP-APIGW-API-KEY-ID': '12rhzhzq7g',
+    'X-NCP-APIGW-API-KEY': 'FhD45P91TxG2820MadrsiPOUjI6bQMJhddnHZIeI'
+  }
+};
+
+const ID = '12rhzhzq7g';
+const KEY = 'FhD45P91TxG2820MadrsiPOUjI6bQMJhddnHZIeI';
+
+function direction(data){
+    var num = data.number;
+    for (let i = 0; i< num;i++){
+    const _url = 'https://naveropenapi.apigw.ntruss.com/map-direction-15/v1/driving?start='+data.current_address.x+','+data.current_address.y+'&goal='+data.hospital_data[i].x+','+data.hospital_data[i].y+'&option=trafast';
+    options.url = _url;
+
+    request(options,function(error, respose){
+            var databody = JSON.parse(respose.body);
+            var distance = databody.route.trafast[0].summary.distance/1000; // km 단위
+            var duration = databody.route.trafast[0].summary.duration/1000/60; // 분 단위dy
+            data.hospital_data[count].distance = distance;
+            data.hospital_data[count].duration = duration;
+            }
+        )
+    }
+    return data}
+console.log(direction(address))
